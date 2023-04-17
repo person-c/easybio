91734b052cc5f9486a23652e369edf0d9366e1f8
snowGlint <cosm.de.c@gmail.com>
2023-04-17 16:33:53 +0800

    添加download_geo函数

---
+++ R/download_geo.R
@@ -0,0 +1,114 @@
+#' download GEO data
+#'
+#' A function to download GEO data and convert it to a standard expression
+#' if existed; It will try to download the supplementary tabular data if no
+#' expression data checked.
+#'
+#' @param geo GSE ID.
+#' @param dir download directory.
+#' @param method ways to process many probes to one symbol(max or mean).
+#' @param filter_regex regex keywords to decide what kind of supplementary file
+#' to download.
+#'
+#' @return a list containing expression matrix and metadata.
+#' @importFrom utils download.file
+#' @export
+#' @examples
+#' # ADD_EXAMPLES_HERE
+download_geo <- function(geo, dir, method = "max", filter_regex = NULL) {
+
+# download GEO data
+eset <- GEOquery::getGEO(GEO = geo, destdir = dir, getGPL = FALSE)
+
+# get expression matrix
+exp <- Biobase::exprs(eset[[1]])
+
+# check whether the GSE data are high throught data
+if (dim(exp)[[1]] == 0) {
+  message(
+    sprintf("%s seems not array data;
+      Trying to download supplementary files.", geo))
+
+  stub <- gsub("\\d{1,3}$", "nnn", geo, perl = TRUE)
+  mirror <- sprintf("https://ftp.ncbi.nlm.nih.gov/geo/series/%s/%s/suppl/",
+            stub, geo)
+  h <- rvest::read_html(mirror)
+
+  nodes <- rvest::html_nodes(h, "a")
+  a <- rvest::html_attr(nodes, "href")
+
+  if (!is.null(filter_regex)) {
+    index <- which(grepl(pattern = filter_regex, a) == TRUE)
+  } else {
+    index <- which(grepl(pattern = ".gz|xls|csv", a) == TRUE)
+  }
+
+  destfile_url <- paste(mirror, a[index], sep = "")
+  purrr::walk2(destfile_url, a[index], download.file)
+
+  return(Biobase::pData(eset[[1]]))
+}
+# download GPL annotation and convert probe_id to symbol
+gpl_annotation <- GEOquery::getGEO(eset[[1]]@annotation, destdir = ".")
+gpl_annotation <- GEOquery::Table(gpl_annotation)
+
+if (purrr::some( #check gene_assignment;if existence, get symbol from assignment
+      colnames(gpl_annotation),
+      ~ stringr::str_detect(string = .x, pattern = "gene_assignment"))) {
+
+    gene_symbol <- strsplit(x = gpl_annotation$gene_assignment, "//")
+    gene_symbol <- purrr::map_chr(gene_symbol, ~ `[`(.x, 2))
+    ids <- data.frame(probe_id = gpl_annotation[[1]], symbol = gene_symbol)
+    ids <- stats::na.omit(ids)
+
+  } else {
+    if (purrr::some( # get symbol column
+      colnames(gpl_annotation),
+      ~ stringr::str_detect(string = .x, pattern = "[Ss]ymbol"))) {
+
+      ids <- gpl_annotation[,
+        c(1, which(stringr::str_detect(colnames(gpl_annotation),
+          pattern = "[Ss]ymbol") == TRUE))]
+      ids <- stats::na.omit(ids)
+      names(ids) <- c("probe_id", "symbol")
+
+      } else {
+        warning(sprintf("%s: Thers are no columns containing string [Ss]ymbol
+        in the GPL annotation; try to check the GPL annotation's column names",
+        geo)
+        )
+
+        return(list(exprMatrix = exp, metaData = Biobase::pData(eset[[1]])))
+      }
+  }
+
+exp <- exp[rownames(exp) %in% ids$probe_id, ]
+ids <- ids[match(rownames(exp), ids$probe_id), ]
+
+# a symbol corresponds to many probe
+# choose the max row mean probe or get the mean vale of these probe
+if (method == "max") {
+  tmp <- by(exp, ids$symbol,
+    function(x) rownames(x)[which.max(rowMeans(x))])
+
+  probes <- as.character(tmp)
+
+  exp <-  exp[rownames(exp) %in% probes, ]
+  rownames(exp) <- ids[match(rownames(exp), ids$probe_id), 2]
+
+  # get sample informaiton
+  pd <- Biobase::pData(eset[[1]])
+
+  return(list(exprMatrix = exp, metaData = pd))
+}
+
+if (method == "mean") {
+  tmp <- by(exp, ids$symbol,
+    function(x) colSums(x) / nrow(x))
+
+  exp <- rlang::exec("rbind", !!!tmp)
+  pd <- Biobase::pData(eset[[1]])
+
+  return(list(exprMatrix = exp, metaData = pd))
+}
+}
