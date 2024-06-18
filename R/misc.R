12c80792bf916191c07659fc5fdbdee5373c74dc
cw <m2c.w@outlook.com>
2024-06-18 11:22:44 +0800

    let finsert accept index 0

--- R/misc.R
+++ R/misc.R
@@ -35,14 +35,14 @@ list2dt <- function(x) {
   data.table(name = rep(names(x), sapply(x, length)), value = unlist(x))
 }
 
-#' Insert character value in specified index  for character in a weird way.
+#' Insert character value in specified index  for character in a weird way(Used only for single cell annotation).
 #'
 #' @param x expression
 #' @param na the default value for the NA.
 #'
 #' @return characte
 #' @export
-finsert <- function(x = expression(c(1, 3) == "a", c(2, 4) == "b"), na = "Unknown") {
+finsert <- function(x = expression(c(0, 1, 3) == "a", c(2, 4) == "b"), na = "Unknown") {
   x <- eval(substitute(x))
   x <- lapply(x, as.list)
   x <- rapply(x, eval, classes = "call", how = "replace")
@@ -50,7 +50,7 @@ finsert <- function(x = expression(c(1, 3) == "a", c(2, 4) == "b"), na = "Unknow
   itor <- 1
   v <- character()
   while (itor < length(x)) {
-    v[x[[itor + 1]]] <- x[[itor + 2]]
+    v[x[[itor + 1]] + 1] <- x[[itor + 2]]
     itor <- itor + 3
   }
   v[is.na(v)] <- na
