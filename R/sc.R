399fc2bda2b3a3c958daf77d5edfe6df4da75dee
cw <m2c.w@outlook.com>
2024-07-08 14:52:57 +0800

    add check marker function

--- R/sc.R
+++ R/sc.R
@@ -25,3 +25,31 @@ matchCellMarker2 <- function(marker, n, spc) {
   setattr(marker, "topmarker", topmarker)
   marker
 }
+
+#' Check markers from cell returned by `matchCellMarker`
+#'
+#' @param marker markers from `Seurat::FindAllMarkers`
+#' @param n top number of genes to match.
+#' @param spc 'Human' or 'Mouse'.
+#' @param cl cluster you want to check.
+#'
+#' @return matched cellMarker data.frame
+#' @import data.table
+#' @export
+check_marker <- function(marker, n, spc, cl = c()) {
+  marker_matched <- easybio::matchCellMarker2(marker, n, spc)
+  marker_top <- na.omit(attributes(marker_matched)[["topmarker"]])
+  marker_top <- unique(marker_top, by = "Symbol")
+  marker_top <- marker_top[
+    count > 1, .(Symbol = .(Symbol)),
+    by = .(cell_name)
+  ]
+
+  cell_on_cluster <- marker_matched[.(factor(cl)), head(.SD, 5), by = .(cluster)]
+  print(cell_on_cluster)
+
+  cell <- marker_matched[.(factor(cl)), head(.SD, 2), by = .(cluster)][, unique(cell_name)]
+  marker_top <- marker_top[.(cell), .SD, on = .(cell_name)]
+  cell_features <- setNames(marker_top$Symbol, make.names(marker_top$cell_name))
+  cell_features
+}
