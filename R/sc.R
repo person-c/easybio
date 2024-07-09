34befd8d4960b8dec143c7d7b1fdfee1a347b7c1
cw <m2c.w@outlook.com>
2024-07-09 21:55:11 +0800

    short line

--- R/sc.R
+++ R/sc.R
@@ -33,7 +33,9 @@ matchCellMarker2 <- function(marker, n, spc) {
   setDT(marker)
 
   cellMarker2 <- subset(cellMarker2, species == spc)
-  marker <- marker[avg_log2FC > 0 & p_val_adj < 0.05, .SD[order(-avg_log2FC)][1:n], keyby = .(cluster)][cellMarker2, on = "gene==Symbol", nomatch = NULL]
+  marker <- marker[avg_log2FC > 0 & p_val_adj < 0.05, .SD[order(-avg_log2FC)][1:n],
+    keyby = .(cluster)
+  ][cellMarker2, on = "gene==Symbol", nomatch = NULL]
   marker <- marker[, .(markerWith = .(gene), N = .N), by = .(cluster, cell_name)]
   marker <- marker[N > 0, .SD[order(-N)], keyby = .(cluster)]
 
