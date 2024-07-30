06deb946e5a77e9a05c4cd9e065bf240e81f2402
cw <m2c.w@outlook.com>
2024-07-30 08:43:13 +0800

    update cellMarker2

--- data-raw/cellmarker2.R
+++ data-raw/cellmarker2.R
@@ -4,5 +4,7 @@ download.file("http://117.50.127.228/CellMarker/CellMarker_download_files/file/C
 
 x <- readxl::read_xlsx("data-raw/Cell_marker_Seq.xlsx")
 cellMarker2 <- data.table::setDT(x)
+cellMarker2[species == "Human", let(marker = toupper(marker))]
+cellMarker2[species == "Mouse", let(marker = gsub("(^[[:alpha:]])", "\\U\\1", tolower(marker), perl = TRUE))]
 usethis::use_data(cellMarker2, internal = TRUE, overwrite = TRUE)
 file.remove("data-raw/Cell_marker_Seq.xlsx")
