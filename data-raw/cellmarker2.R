0b640c62d98530e8cd03dfb895bd4ccfb8007f94
cw <m2c.w@outlook.com>
2024-08-02 13:41:09 +0800

    update cellMarker2.0

--- data-raw/cellmarker2.R
+++ data-raw/cellmarker2.R
@@ -6,5 +6,9 @@ x <- readxl::read_xlsx("data-raw/Cell_marker_Seq.xlsx")
 cellMarker2 <- data.table::setDT(x)
 cellMarker2[species == "Human", let(marker = toupper(marker))]
 cellMarker2[species == "Mouse", let(marker = gsub("(^[[:alpha:]])", "\\U\\1", tolower(marker), perl = TRUE))]
+cellMarker2[species == "Human", let(Symbol = toupper(Symbol))]
+cellMarker2[species == "Mouse", let(Symbol = gsub("(^[[:alpha:]])", "\\U\\1", tolower(Symbol), perl = TRUE))]
+cellMarker2[, let(Symbol = fcoalesce(Symbol, marker))]
+setnames(cellMarker2, c("marker", "Symbol"), c("raw_marker", "marker"))
 usethis::use_data(cellMarker2, internal = TRUE, overwrite = TRUE)
 file.remove("data-raw/Cell_marker_Seq.xlsx")
